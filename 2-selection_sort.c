06be6d8f9f08bf656dd8e54f09c9f94cca62465c
bakiizese <bereketzeselassie@gmail.com>
2023-06-25 03:11:01 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,33 @@
 #include "sort.h"
-
+/**
+ * selection_sort - entry
+ * @array: array
+ * @size: var
+ */
 void selection_sort(int *array, size_t size)
 {
+	size_t ls, i, c, h, sw;
 
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (ls = i, c = i; c < size; c++)
+		{
+			if (array[c] < array[ls])
+			{
+				ls = c;
+				sw = 1;
+			}
+		if (sw == 1)
+		{
+			h = array[ls];
+			array[ls] = array[i];
+			array[i] = h;
+			print_array(array, size);
+			sw = 0;
+		}
+		}
+	}
+}
