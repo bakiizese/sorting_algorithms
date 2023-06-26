c3406e5b48520f6f50b79711d550f68785b3d6c6
bakiizese <bereketzeselassie@gmail.com>
2023-06-25 23:03:22 -0700

    fix

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef __SORT_H__
 #define __SORT_H__
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -24,5 +25,13 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 #endif
