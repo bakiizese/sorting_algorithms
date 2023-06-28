afedaef023f75930731de0791fe770c59fa28dd4
bakiizese <bereketzeselassie@gmail.com>
2023-06-27 19:35:10 -0700

    fix

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,65 +1,62 @@
 #include "sort.h"
 
 /**
- * swap_root - A function that swap the root nodes.
- * @array: The heap to sort.
- * @root: The root of the heap.
- * @hi: The higher index.
- * @size: The size of the array.
- * Return: Nothing
+ * swap - entry
+ * @array: var
+ * @rt: var
+ * @rh: var
+ * @size: var
+ * Return: nun
  */
-void swap_root(int *array, size_t root, size_t hi, size_t size)
+void swap(int *array, size_t rt, size_t rh, size_t size)
 {
-	size_t lo = 0, mi = 0, tmp = 0;
-	int aux = 0;
+	size_t lt = 0, md = 0, mp = 0, x = 0;
 
-	while ((lo = (2 * root + 1)) <= hi)
+	while ((lt = (2 * rt + 1)) <= rh)
 	{
-		tmp = root;
-		mi = lo + 1;
-		if (array[tmp] < array[lo])
-			tmp = lo;
-		if (mi <= hi && array[tmp] < array[mi])
-			tmp = mi;
-		if (tmp == root)
+		mp = rt;
+		md = lt + 1;
+		if (array[mp] < array[lt])
+			mp = lt;
+		if (md <= rh && array[mp] < array[md])
+			mp = md;
+		if (mp == rt)
 			return;
-		aux = array[root];
-		array[root] = array[tmp];
-		array[tmp] = aux;
+		x = array[rt];
+		array[rt] = array[mp];
+		array[mp] = x;
 		print_array(array, size);
-		root = tmp;
+		rt = mp;
 	}
 }
 
 /**
- * heap_sort - A function that sorts an array using heap algorithm.
- * @array: An array to sort.
- * @size: The size of the array.
- * Return: Nothing.
+ * heap_sort - entry algo
+ * @array: var
+ * @size: var
  */
 void heap_sort(int *array, size_t size)
 {
-	size_t hi = 0, gap = 0;
-	int tmp = 0;
+	size_t rh = 0, gap = 0;
+	int mp = 0;
 
-		if (array == NULL || size < 2)
-			return;
+	if (array == NULL || size < 2)
+		return;
 
-		for (gap = (size - 2) / 2; 1; gap--)
-		{
-			swap_root(array, gap, size - 1, size);
-			if (gap == 0)
-				break;
-		}
-
-		hi = size - 1;
-		while (hi > 0)
+	for (gap = (size - 2) / 2; 1; gap--)
 	{
-		tmp = array[hi];
-		array[hi] = array[0];
-		array[0] = tmp;
+		swap(array, gap, size - 1, size);
+		if (gap == 0)
+			break;
+	}
+	rh = size - 1;
+	while (hi > 0)
+	{
+		mp = array[rh];
+		array[rh] = array[0];
+		array[0] = mp;
 		print_array(array, size);
-		hi--;
-		swap_root(array, 0, hi, size);
+		rh--;
+		swap(array, 0, rh, size);
 	}
 }
